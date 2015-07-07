a02e3634b0d2961d042b252e7e4879cb6f9ad860
James Johnstone <james.johnstone@geeks.ltd.uk>
2015-07-07 15:04:46 +0100

    Tidy indentation

--- assets/js/controllers/saguController.js
+++ assets/js/controllers/saguController.js
@@ -10,80 +10,77 @@ angular.module('saguApp').controller('saguController', function ($scope, $timeou
         $scope.getNextArea();
     });
 
-    $scope.getNextArea = function(){
-      var unCompletedAreas = $scope.explorer.exploredAreas.filter(function (area) {
-          return area.amountExplored < area.area.size;
-      });
+    $scope.getNextArea = function () {
+        var unCompletedAreas = $scope.explorer.exploredAreas.filter(function (area) {
+            return area.amountExplored < area.area.size;
+        });
 
-      if (unCompletedAreas.length > 0) {
-          $scope.currentArea = Array.maxObject(unCompletedAreas, "area", "order");
-          $scope.init();
-      }
-      else {
-          explorerService.getNextArea($scope.explorer).then(function (area) {
-              $scope.currentArea = area;
-              $scope.explorer.exploredAreas.push(area);
-              $scope.init();
-          });
-      }
-    }
-
-    $scope.init = function(){
-      $scope.backgroundImage = $scope.getImageUri($scope.currentArea.area.image);
-      $scope.map = $scope.getMap();
-      $scope.explore();
-    }
-
-    $scope.explore = function(){
-      if ( $scope.exploreProgress >= 100){
-          $scope.addNewRoom();
+        if (unCompletedAreas.length > 0) {
+            $scope.currentArea = Array.maxObject(unCompletedAreas, "area", "order");
+            $scope.init();
+        } else {
+            explorerService.getNextArea($scope.explorer).then(function (area) {
+                $scope.currentArea = area;
+                $scope.explorer.exploredAreas.push(area);
+                $scope.init();
+            });
         }
-      else {
-        $scope.exploreProgress +=15;
-      }
+    }
 
-      $timeout($scope.explore, 1500);
+    $scope.init = function () {
+        $scope.backgroundImage = $scope.getImageUri($scope.currentArea.area.image);
+        $scope.map = $scope.getMap();
+        $scope.explore();
+    }
+
+    $scope.explore = function () {
+        if ($scope.exploreProgress >= 100) {
+            $scope.addNewRoom();
+        } else {
+            $scope.exploreProgress += 15;
+        }
+
+        $timeout($scope.explore, 1500);
     };
 
-    $scope.addNewRoom = function(){
-      $scope.updateExploredAmount();
+    $scope.addNewRoom = function () {
+        $scope.updateExploredAmount();
 
-      if ($scope.currentArea.amountExplored >= $scope.currentArea.area.size ){
-        $scope.getNextArea();
-      }
-      else {
-        $scope.exploreProgress = 0;
-        $scope.map += $scope.getRoom();
-        $scope.updateContainerScroll();
-      }
+        if ($scope.currentArea.amountExplored >= $scope.currentArea.area.size) {
+            $scope.getNextArea();
+        } else {
+            $scope.exploreProgress = 0;
+            $scope.map += $scope.getRoom();
+            $scope.updateContainerScroll();
+        }
     }
 
-    $scope.updateExploredAmount = function(){
-      $scope.currentArea.amountExplored ++;
-      explorerService.updateLocalExplorer($scope.explorer);
+    $scope.updateExploredAmount = function () {
+        $scope.currentArea.amountExplored++;
+        explorerService.updateLocalExplorer($scope.explorer);
     }
 
-    $scope.getMap = function(){
-      var map = '';
+    $scope.getMap = function () {
+        var map = '';
 
-      for (var i = 0; i < $scope.currentArea.amountExplored; i++) {
-        map += $scope.getRoom();
-      }
+        for (var i = 0; i < $scope.currentArea.amountExplored; i++) {
+            map += $scope.getRoom();
+        }
 
-      return map;
+        return map;
     }
 
-    $scope.getRoom = function(){
-      return "<div class='room'>//</div>";
+    $scope.getRoom = function () {
+        return "<div class='room'>//</div>";
     }
 
-    $scope.updateContainerScroll = function(){
-      var container = document.getElementsByClassName('map-container')[0];
-      container.scrollTop = container.scrollHeight;
+    $scope.updateContainerScroll = function () {
+        var container = document.getElementsByClassName('map-container')[0];
+        container.scrollTop = container.scrollHeight;
     }
 
-    $scope.getImageUri = function(image){
-      var fileName =  image.fileName.split('.');
-      return image.id + '.' + fileName[fileName.length -1];
+    $scope.getImageUri = function (image) {
+        var fileName = image.fileName.split('.');
+        return image.id + '.' + fileName[fileName.length - 1];
     }
 });
